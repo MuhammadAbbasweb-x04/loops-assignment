467766197255b7f1ee6b96442313552f8d49f181
MuhammadAbbas-web04 <mabbasmshahi2003@gmail.com>
2025-08-10 16:24:27 +0500

    added

---
+++ app.js
@@ -0,0 +1,88 @@
+// Q1
+// Answer
+
+// let fruits = ["apple", "banana", "cherry"]
+
+// for (let i = 0; i < fruits.length; i++){
+// console.log(fruits[i]);
+
+// }
+
+
+// Q2
+// Answer
+
+// let colors = ["red","blue","green"];
+// let para = document.querySelector("#output");
+
+// for(let i = 0; i < colors.length; i++){
+// para.innerHTML = "red  blue  green"
+
+// }
+
+
+
+// Q3
+// Answer
+// for(let i = 1; i < 11; i++){
+
+// console.log(i);
+
+// }
+
+// Q4
+// Answer
+// let i = 1;
+// while(i<6){
+// console.log(i);
+// i++
+// }
+
+// Q5
+// Answer
+// let user = ["Ali", "Sara", "Ahmed"];
+//  let box =document.querySelector("#names");
+// for(let i = 0; i < user.length; i++ ){
+// console.log([i]);
+// box.innerHTML = user.join(",") ;
+// box.innerHTML = "abuzar abbas ahemd"
+
+
+// }
+
+
+// Q6
+// Answer
+// let para= document.querySelector("#status");
+// para.innerHTML = "Loading...";
+
+
+
+// Q7
+// Answer
+// let result = document.querySelector("#sum");
+// let num = [5,10,15];
+// let sum = 0; 
+// for(let i = 0; i<num.length;i++){
+//     sum +=num[i];
+//     result.innerHTML = "<p>" + sum + "</p>"
+// }
+
+
+
+// Q8
+// Answer
+// let box = document.querySelector("#cities");
+// let cite = ["Karachi", "Lahore", "Quetta"];
+
+// for(let i = 0; i < cite.length; i++){
+// box.innerHTML += cite[i] + ' ' ;
+
+
+// }
+
+
+
+
+
+
